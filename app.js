86138a0b8d7e420750323033d32283b282d8a6aa
isaiful508 <isaiful.islam508@gmail.com>
2024-03-02 10:41:51 +0600

    added button id for news

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const loadCategory= async() =>{
     const response = await fetch(' https://openapi.programming-hero.com/api/news/categories');
     const data = await response.json();
-    console.log(data.data.news_category
-        );
+
+    // console.log(data.data.news_category
+    //     );
+
     const categoryContainer = document.getElementById('category-bar-container');
 
         data.data.news_category.forEach((item) => {
             const div = document.createElement("div");
             div.innerHTML = `
-            <button>${item.category_name}</button>
+            <button onclick="loadNews('${item.category_id}')"> ${item.category_name}</button>
             `;
             categoryContainer.appendChild(div);
 
@@ -19,5 +21,87 @@ const loadCategory= async() =>{
 
 
 
+
+
+
+const loadNews = async (catId) => {
+    console.log(catId);
+
+    const response = await fetch(` https://openapi.programming-hero.com/api/news/category/${catId}`);
+    const data = await response .json();
+
+    const newsContainer =document.getElementById('news-container');
+    newsContainer.innerHTML ='';
+  data.data.forEach((news) => {
+    // console.log(news);
+  const div = document.createElement('div');
+  div.classList.add('singleNews');
+
+    div.innerHTML=`
+    <div class="news-photo">
+    <img
+      src=
+      alt=""
+    />
+  </div>
+  <div class="news-info">
+    <div class="news-header">
+      <h4>${news.title}</h4>
+      <p class="news-badge">
+      ${news.rating.badge
+      }
+      <sup> 
+      <h6 class="news-rating"> 
+      ${news.rating.number
+      }
+      </h6>
+</sup>
+      </p>
+    </div>
+    <p>
+   ${news.details.slice(0,200)}
+    </p>
+
+    <div class="news-footer">
+      <div class="author">
+        <div class="">
+          <img
+            class="author-img"
+            src=
+            alt=""
+          />
+        </div>
+        <div class="author-info">
+          <h6> </h6>
+          <p>Date:</p>
+        </div>
+      </div>
+      <div class="Views author">
+        <img
+          class="view-img"
+          src="https://uxwing.com/wp-content/themes/uxwing/download/health-sickness-organs/view-icon.png"
+          alt=""
+        />
+        <p>450</p>
+      </div>
+      <div class="details-btn-container">
+        <button  class="details-btn">
+        Details
+        </button>
+      </div>
+  </div>
+</div>
+    `
+newsContainer.appendChild(div);
+
+  })
+}
+
+
+
+
+
+
+loadNews("01");
 
 loadCategory();
